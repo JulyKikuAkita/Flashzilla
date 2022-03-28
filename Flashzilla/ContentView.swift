e9d5ac6cd47dc8f3e4447d6a5b603b566ebbad3f
JulyKikuAkita <b92701105@gmail.com>
2022-03-27 22:41:10 -0700

    Day 87: Triggering events repeatedly using a timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,109 +8,24 @@
  Core Haptics lets us create hugely customizable haptics by combining taps, continuous vibrations, parameter curves, and more
  */
 import SwiftUI
-import CoreHaptics
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
+    // (Run loops lets iOS handle running code while the user is actively doing something, such as scrolling in a list.)
+    // timer coalescing: it can push back your timer just a little so that it fires at the same time as one or more other timers, which means it can keep the CPU idling more and save battery power.
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
     var body: some View {
         VStack {
-            Text("sucess haptic")
+            Text("Publish timer")
                 .padding()
-                .onTapGesture(perform: simpleSuccess)
-
-            Text("error haptic")
-                .padding()
-                .onTapGesture(perform: simpleError)
-
-            Text("warning haptic")
-                .padding()
-                .onTapGesture(perform: simpleSuccess)
-
-            Text("dull core haptic")
-                .padding()
-//                .onAppear(perform: prepareHaptics)
-                .onTapGesture(perform: dullCoreHaptics)
-
-            Text("Custom core haptic")
-                .padding()
-                .onTapGesture(perform: customCoreHaptics)
-        }
-        .onAppear(perform: prepareHaptics)
-
-    }
-
-    // UINotificationFeedbackGenerator
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        // Try replacing .success with .error or .warning to see the difference
-        generator.notificationOccurred(.success)
-    }
-
-    func simpleError() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.error)
-    }
-
-    func simpleWarning() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
-
-
-    //core haptics
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-
-    func dullCoreHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        events.append(event)
-
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-        }
-    }
-
-    func customCoreHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
+                .onReceive(timer) { time in
+                    if counter == 5 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    counter += 1
+                }
         }
     }
 }
