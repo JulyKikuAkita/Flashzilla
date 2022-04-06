8128f024e13b32075fc5b201682b36088741d593
JulyKikuAkita <b92701105@gmail.com>
2022-04-05 23:12:36 -0700

    Day 88: Counting down with a Timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -27,8 +27,11 @@ struct checkMarkCircleatBottomView: View {
 }
 struct ContentView: View {
     @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var timeRemaining = 100
+    @State private var isActive = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-
+    @Environment(\.scenePhase) var scenePhase
     var body: some View {
         ZStack {
             Image("background")
@@ -50,7 +53,29 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+
+                Text("Time \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
             }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+
         }
     }
 
