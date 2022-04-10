037976e5f862b40a6b6e4d8107b0835b5d3ea2ff
JulyKikuAkita <i-fang.lee@grindr.com>
2022-04-10 11:58:01 -0700

    Day 90: fix bounce animation and voice over accessibility tags

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -5,13 +5,11 @@
 //  Created by Ifang Lee on 4/2/22.
 //
 
-/**
+/*
  About prepare() UINotificationFeedbackGenerator
  First, it’s OK to call prepare() then never triggering the effect – the system will keep the Taptic Engine ready for a few seconds then just power it down again. If you repeatedly call prepare() and never trigger it the system might start ignoring your prepare() calls until at least one effect has happened.
 
  Second, it’s perfectly allowable to call prepare() many times before triggering it once – prepare() doesn’t pause your app while the Taptic Engine warms up, and also doesn’t have any real performance cost when the system is already prepared.
-
-
  */
 import SwiftUI
 
@@ -22,7 +20,7 @@ struct CardView: View {
     @State private var offset = CGSize.zero
     @State private var feedback = UINotificationFeedbackGenerator() // if add this to content view, there's not time to call prepare()
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
@@ -41,14 +39,20 @@ struct CardView: View {
                 .shadow(radius: 10)
 
             VStack {
-                Text(card.promt)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
+                if voiceOverEnabled { //double tap to reach out the answer
+                    Text(isShowingAnswer ? card.answer : card.promt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                } else {
+                    Text(card.promt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
 
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
                 }
             }
             .padding(20)
@@ -58,6 +62,7 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width) / 5)) //the order of rotation, offset matters
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width) / 50 )) // beyond 50 points we start to fade out the card, until at 100 points left or right the opacity is 0.
+        .accessibilityAddTraits(.isButton) // voice over: card is button
         .gesture(
             DragGesture()
                 .onChanged { gesture in
@@ -81,6 +86,7 @@ struct CardView: View {
         .onTapGesture {
             isShowingAnswer.toggle()
         }
+        .animation(.spring(), value: offset)
     }
 }
 
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,39 +8,70 @@
 import SwiftUI
 
 struct checkMarkCircleatBottomView: View {
+    var removeCard: () -> Void //callback for func removeCard(at index: Int)
     var body: some View {
         HStack {
-            Image(systemName: "xmark.circle")
-                .padding()
-                .background(.black.opacity(0.7))
-                .clipShape(Circle())
+            Button {
+                withAnimation {
+                    removeCard()
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .padding()
+                    .background(.black.opacity(0.7))
+                    .clipShape(Circle())
+            }
+            .accessibilityLabel("Wrong")
+            .accessibilityHint("Mark your answer as being incorrect.")
+           
             Spacer()
-            Image(systemName: "checkmark.circle")
-                .padding()
-                .background(.black.opacity(0.7))
-                .clipShape(Circle())
+            
+            Button {
+                withAnimation {
+                    removeCard()
+                }
+            } label: {
+                Image(systemName: "checkmark.circle")
+                    .padding()
+                    .background(.black.opacity(0.7))
+                    .clipShape(Circle())
+            }
+            .accessibilityLabel("Correct")
+            .accessibilityHint("Mark your answer as being correct.")
         }
         .foregroundColor(.white)
         .font(.largeTitle)
         .padding()
     }
 }
+
+
+/*
+ Issues for voice over regarding gestures
+
+ We don’t say that the cards are buttons that can be tapped.
+ When the answer is revealed there is no audible notification of what it was.
+ Users have no way of swiping left or right to move through the cards.
+ 
+ */
 struct ContentView: View {
     @State private var cards = [Card](repeating: Card.example, count: 10)
     @State private var timeRemaining = 100
     @State private var isActive = true
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
     @Environment(\.scenePhase) var scenePhase
+    
     var body: some View {
         ZStack {
-            Image("background")
+            Image(decorative: "background")
                 .resizable()
                 .ignoresSafeArea()
             VStack {
-                if differentiateWithoutColor {
+                if differentiateWithoutColor || voiceOverEnabled {
                     Spacer()
-                    checkMarkCircleatBottomView()
+                    checkMarkCircleatBottomView { removeCard(at: cards.count - 1) }
                 }
 
                 ZStack {
@@ -51,6 +82,8 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1) //only tap the top card
+                        .accessibilityHidden(index < cards.count - 1) // voice over ignore the other cards
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
@@ -91,6 +124,7 @@ struct ContentView: View {
     }
 
     func removeCard(at index: Int) {
+        guard index >= 0 else { return }
         cards.remove(at: index)
         if cards.isEmpty {
             isActive = false
